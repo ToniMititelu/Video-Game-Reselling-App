a3c96514db58d026fccc5edb96e6fbff434ca01f
tonimiti <mititeluantonio@gmail.com>
2021-01-07 23:59:49 +0200

    add authentication and authorization

---
+++ ProjectDAW/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectDAW.Data;
+
+[assembly: HostingStartup(typeof(ProjectDAW.Areas.Identity.IdentityHostingStartup))]
+namespace ProjectDAW.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- ProjectDAW/Controllers/GamesController.cs
+++ ProjectDAW/Controllers/GamesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,7 @@ namespace ProjectDAW.Controllers
         }
 
         // GET: Games
+        [Authorize(Policy = "readpolicy")]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Game.ToListAsync());
